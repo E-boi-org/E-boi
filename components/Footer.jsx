2ebfaab794f32a0c3b4469d111e2caf14b43ac99
Rui Duram <ruiduran.jr@gmail.com>
2024-02-15 17:56:37 +0100

    first few components and images

---
+++ components/Footer.jsx
@@ -0,0 +1,12 @@
+import { Route } from "react-router-dom";
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <div className="Footer"> my name is footer</div>
+    </>
+  );
+}
+
+export default Footer;
---
+++ components/Header.jsx
@@ -0,0 +1,14 @@
+import { Route } from "react-router-dom";
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <header className="header">
+        <img src="./Images/E-boi-logo.png" alt="logo" className="imageLogo" />
+      </header>
+    </>
+  );
+}
+
+export default Header;
