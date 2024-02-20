3ac1c3194394f63522af75389d96502a35d80319
João Dias <152170826+jvfd1983@users.noreply.github.com>
2024-02-20 10:54:13 +0000

    Footer Update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,15 @@
-import { Route } from "react-router-dom";
 import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
-    <>
-      <div className="Footer"> my name is footer</div>
-    </>
+    <div className="Footer">
+      <p className="Copyright">Copyright © 2024</p>
+      <ul className="Footer-links">
+        <li><a href="https://github.com/RuiDuram" target="_blank" rel="noopener noreferrer">Rui Duram</a></li>
+        <li><a href="https://github.com/jvfd1983" target="_blank" rel="noopener noreferrer">João Dias</a></li>
+      </ul>
+    </div>
   );
 }
 
